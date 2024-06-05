c2de218c71f880353424efefd6e2c97889d45b88
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2024-06-05 09:00:57 -0700

    code for 2 button reaction time game

---
+++ reaction_time_2_button.ino
@@ -0,0 +1,128 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int BUTTON_PIN_1 = 10;
+const int BUTTON_PIN_2 = 12;
+
+const int LED_PIN_1 = 2;
+const int LED_PIN_2 = 4;
+
+long clickStartTime = -1;
+long timerStartTime = 0;
+long delayTime = random(1000, 3000);
+bool buttonPressed = false;
+
+int targetLed = -1;
+
+void setup() {
+  randomSeed(analogRead(0));
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("WAIT FOR LIGHT!");
+
+  pinMode(BUTTON_PIN_1, INPUT);
+  pinMode(BUTTON_PIN_2, INPUT);
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
+  timerStartTime = millis();
+}
+
+int getPressedButton() {
+  while (true) {
+    if (buttonPressed) {
+      if (digitalRead(BUTTON_PIN_1) == LOW && digitalRead(BUTTON_PIN_2) == LOW) {
+        buttonPressed = false;
+      }
+      continue;
+    }
+
+    buttonPressed = true;
+
+    if (digitalRead(BUTTON_PIN_1) == HIGH) {
+      return 1;
+    }
+    if (digitalRead(BUTTON_PIN_2) == HIGH) {
+      return 2;
+    }
+  }
+  return -1; // shouldn't be possible
+}
+
+void waitForButtonInput() {
+  int pressedButton = getPressedButton();
+
+  if (pressedButton == targetLed) {
+    lcd.clear();
+    lcd.print("YOUR TIME: ");
+    lcd.setCursor(0, 1);
+
+    lcd.print(millis() - clickStartTime);
+  } else {
+    lcd.clear();
+    lcd.print("WRONG BUTTON PRESSED");
+  }
+}
+
+void waitForButtonClick() {
+  // wait for release
+  while (true) {
+    if (digitalRead(BUTTON_PIN_1) == LOW && digitalRead(BUTTON_PIN_2) == LOW) {
+      break;
+    }
+  }
+  // wait for press
+  while (true) {
+    if (digitalRead(BUTTON_PIN_1) == HIGH || digitalRead(BUTTON_PIN_2) == HIGH) {
+      break;
+    }
+  }
+  buttonPressed = true;
+}
+
+void reset() {
+  waitForButtonClick();
+
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, LOW);
+
+  lcd.clear();
+  lcd.print("WAIT FOR LIGHT!");
+
+  clickStartTime = -1;
+  delayTime = random(1000, 3000);
+  timerStartTime = millis();
+}
+
+void loop() {
+  if (millis() >= timerStartTime + delayTime && clickStartTime < 0) {
+    lcd.setCursor(0, 0);
+    lcd.print("CLICK!          ");
+    clickStartTime = millis();
+
+    targetLed = random(2) + 1; // 1 or 2
+
+    if (targetLed == 1) {
+      digitalWrite(LED_PIN_1, HIGH);
+    } else {
+      digitalWrite(LED_PIN_2, HIGH);
+    }
+
+    waitForButtonInput();
+
+    digitalWrite(LED_PIN_1, HIGH);
+    digitalWrite(LED_PIN_2, HIGH);
+
+    reset();
+  }
+
+  if (digitalRead(BUTTON_PIN_1) == HIGH || digitalRead(BUTTON_PIN_2) == HIGH) {
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("TOO EARLY       ");
+
+    reset();
+  }
+}
